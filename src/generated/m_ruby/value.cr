63c3a8ed0b6764fcd9806697baaa8ba3e62b9b66
ppibburr <tulnor33@gmail.com>
2016-04-22 05:32:36 -0400

    ...

--- src/generated/m_ruby/value.cr
+++ src/generated/m_ruby/value.cr
@@ -12,12 +12,12 @@ module MRuby
       cast MRuby::Value.new(__return_value)
     end
 
-    def to_string(c)
+    def to_string(c : MRuby::Context = nil)
       __return_value = LibMRuby.value_to_string((to_unsafe as LibMRuby::Value*),  (c.to_unsafe as LibMRuby::Context*))
       (raise "Expected string but got null" unless __return_value; ::String.new(__return_value))
     end
 
-    def context(c : Mruby::Context = nil)
+    def context(c : MRuby::Context = nil)
       __return_value = LibMRuby.value_get_context((to_unsafe as LibMRuby::Value*), c && (c.to_unsafe as LibMRuby::Context*))
       MRuby::Context.new(__return_value)
     end
