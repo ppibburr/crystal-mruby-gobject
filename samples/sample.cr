04e8f4eba9c64a12bfba7a6ae8339a91ff9c868e
ppibburr <tulnor33@gmail.com>
2016-04-22 16:54:51 -0400

    ...

--- samples/sample.cr
+++ samples/sample.cr
@@ -21,7 +21,13 @@ result = mrb.load_string("
 
 puts "\n"
 pp result
-
-pp MRuby.float result
-pp MRuby.string result
+pp MRuby.string(result)
+pp MRuby.float(result)
+pp MRuby.fixnum(result)
 pp MRuby.bool result
+pp MRuby.bool(mrb, false).to_string
+pp MRuby.nil_value().to_string
+pp (ary = MRuby.array(mrb, 1, true, Float32.new(3.3), nil, "Foo", result)).get(0).to_string
+pp ary[2].to_string
+ary[2] = Float32.new(5.5)
+pp ary[2].to_string
